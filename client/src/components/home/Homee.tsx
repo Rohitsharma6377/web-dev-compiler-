0f0d7450a54e8a2b7807c7d3379e09e165703538
Rohit Sharma <lavish637728@gmail.com>
2024-06-10 16:25:35 +0530

    re check
    re check all things and install and cofigure express

--- client/src/components/home/Homee.tsx
+++ client/src/components/home/Homee.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className=' w-full h-100vh bg-black text-white flex justify-center items-center border-2 flex-col'>
-      <h1 className='text-4x1 font-bold'>Web Dev Compiler by Pixels&co.</h1>
+    <div className=' w-full h-svh  bg-black text-white flex justify-center items-center border-2 flex-col'>
+      <h1 className='text-40*1 font-bold'>Web Dev Compiler by Pixels&co.</h1>
     <p className=' text-gray-500 text-center'>
       Compiler Html , Css , Javascript Code on the go and share with your friends
     </p>
